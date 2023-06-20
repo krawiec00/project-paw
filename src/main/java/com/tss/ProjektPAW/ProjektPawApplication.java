444b281b923433724bd083ca55f6573197ae016e
Karol Krawczyk <krawiecc22@gmail.com>
2023-06-20 20:02:53 +0200

    Podstawa

---
+++ src/main/java/com/tss/ProjektPAW/ProjektPawApplication.java
@@ -0,0 +1,13 @@
+package com.tss.ProjektPAW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektPawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjektPawApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tss/ProjektPAW/ProjektPawApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tss.ProjektPAW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektPawApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
