b02689fdefe0ec8fd51af4c02f6664bf90cba937
Karol Krawczyk <krawiecc22@gmail.com>
2023-06-24 17:56:30 +0200

    Logowanie

--- src/main/java/com/tss/ProjektPAW/ProjektPawApplication.java
+++ src/main/java/com/tss/ProjektPAW/ProjektPawApplication.java
@@ -1,9 +1,19 @@
 package com.tss.ProjektPAW;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan(basePackages={"com.tss"})
+@EnableJpaRepositories(basePackages="com.tss.repositories")
+@EnableTransactionManagement
+@EntityScan(basePackages="com.tss.entities")
 public class ProjektPawApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/tss/controllers/HomeResource.java
@@ -0,0 +1,37 @@
+package com.tss.controllers;
+
+import java.util.Collection;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeResource {
+
+   @GetMapping("/")
+    public String home(Authentication authentication) {
+        if (authentication != null && authentication.isAuthenticated()) {
+            // Pobierz rolę zalogowanego użytkownika
+            Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+            if (authorities.stream().anyMatch(auth -> auth.getAuthority().equals("ROLE_ADMIN"))) {
+                return "admin"; // Przekierowanie dla roli "ADMIN" na stronę admina
+            } else if (authorities.stream().anyMatch(auth -> auth.getAuthority().equals("ROLE_USER"))) {
+                return "user"; // Przekierowanie dla roli "USER" na stronę użytkownika
+            }
+        }
+        return "redirect:/login"; // Przekierowanie na stronę logowania w przypadku braku uwierzytelnienia
+    }
+
+    @GetMapping("/user")
+    public String user() {
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin";
+    }
+}
+
